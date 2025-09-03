458b056f9c2aa4566c226a223eba2da65f7d72e6
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-09-03 20:02:49 +0200

    Add tests for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -24,6 +24,21 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode(tag='p', value="This is a paragraph")
         self.assertEqual(repr(node), "HTMLNode(tag='p', value='This is a paragraph', children=None, props=None)")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_span(self):
+        node = LeafNode("span", "Hello, world!", props={"class": "phrase"})
+        self.assertEqual(node.to_html(), '<span class="phrase">Hello, world!</span>')
+
 
 if __name__ == "__main__":
     unittest.main()
