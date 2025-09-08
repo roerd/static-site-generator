7173c5e0f527a9a26b2ba7b11a24f7acda61ed1f
Rörd Hinrichsen <roerdhh@gmail.com>
2025-09-08 02:30:23 +0200

    Implement split_nodes_[image,link] functions

--- src/text_to_textnode.py
+++ src/text_to_textnode.py
@@ -34,3 +34,62 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"\[(.*?)]\((.*?)\)", text)
     return matches
+
+
+def _split_node_image(old_node: TextNode) -> Iterable[TextNode]:
+    if old_node.text_type is TextType.TEXT:
+        original_text = old_node.text
+        matches = extract_markdown_images(original_text)
+        text_sections = []
+        for image_alt, image_src in matches:
+            sections = original_text.split(f"![{image_alt}]({image_src})", 1)
+            text_sections.append(sections[0])
+            original_text = sections[1]
+        result = chain.from_iterable(
+            [TextNode(text, TextType.TEXT), TextNode(text=match[0], text_type=TextType.IMAGE, url=match[1])]
+            for text, match in zip(text_sections, matches)
+        )
+        if original_text:
+            result = list(result)
+            result.append(TextNode(text=original_text, text_type=TextType.TEXT))
+        return result
+    else:
+        return [old_node]
+
+def split_nodes_image(old_nodes):
+    new_nodes = chain.from_iterable(
+        _split_node_image(old_node)
+        for old_node in old_nodes
+    )
+
+    return list(new_nodes)
+
+
+def _split_node_link(old_node: TextNode) -> Iterable[TextNode]:
+    if old_node.text_type is TextType.TEXT:
+        original_text = old_node.text
+        matches = extract_markdown_links(original_text)
+        text_sections = []
+        for link_text, link_href in matches:
+            sections = original_text.split(f"[{link_text}]({link_href})", 1)
+            text_sections.append(sections[0])
+            original_text = sections[1]
+        result = chain.from_iterable(
+            [TextNode(text, TextType.TEXT), TextNode(text=match[0], text_type=TextType.LINK, url=match[1])]
+            for text, match in zip(text_sections, matches)
+        )
+        if original_text:
+            result = list(result)
+            result.append(TextNode(text=original_text, text_type=TextType.TEXT))
+        return result
+    else:
+        return [old_node]
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = chain.from_iterable(
+        _split_node_link(old_node)
+        for old_node in old_nodes
+    )
+
+    return list(new_nodes)
