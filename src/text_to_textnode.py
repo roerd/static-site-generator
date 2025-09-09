4122f17e90550ce5b2339063981c8eef19640752
Rörd Hinrichsen <roerdhh@gmail.com>
2025-09-09 19:34:51 +0200

    Implement text_to_textnodes()

--- src/text_to_textnode.py
+++ src/text_to_textnode.py
@@ -93,3 +93,13 @@ def split_nodes_link(old_nodes):
     )
 
     return list(new_nodes)
+
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    text_nodes = [TextNode(text, text_type=TextType.TEXT)]
+    text_nodes = split_nodes_delimiter(text_nodes, "**", TextType.BOLD)
+    text_nodes = split_nodes_delimiter(text_nodes, "_", TextType.ITALIC)
+    text_nodes = split_nodes_delimiter(text_nodes, "`", TextType.CODE)
+    text_nodes = split_nodes_image(text_nodes)
+    text_nodes = split_nodes_link(text_nodes)
+    return text_nodes
