81e21a63ce0de59ebb4869ebd21134fddc9431b8
Rörd Hinrichsen <roerdhh@gmail.com>
2025-09-10 22:27:18 +0200

    Implement BLockType and block_to_block_type

---
+++ src/block_type.py
@@ -0,0 +1,27 @@
+import re
+from enum import Enum
+from turtledemo.sorting_animate import Block
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(r"#{1,6} ", block):
+        return BlockType.HEADING
+    elif re.fullmatch(r"```.*```", block):
+        return BlockType.CODE
+    elif all(re.match(r">", line) for line in block.split("\n")):
+        return BlockType.QUOTE
+    elif all(re.match(r"- ", line) for line in block.split("\n")):
+        return BlockType.UNORDERED_LIST
+    elif all(re.match(r"\d+. ", line) for line in block.split("\n")):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
