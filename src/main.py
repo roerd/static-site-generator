efc392dfe3a29d8d06825cbd14ca19442b80f1cf
Rörd Hinrichsen <roerdhh@gmail.com>
2025-09-13 18:57:30 +0200

    Generate index.html from content/index.md

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from copy_directory import copy_directory
+from generate_page import generate_page
 
 
 def main():
     copy_directory("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
